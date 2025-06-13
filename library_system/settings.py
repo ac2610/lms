e7890eb68450c4ee476cd5f7594ba17d7ba6e5fc
AnjaliChoudhary99 <anjalichoudhary2610@gmail.com>
2025-06-13 15:42:56 +0530

    config CORS change 2

--- library_system/settings.py
+++ library_system/settings.py
@@ -35,24 +35,25 @@ INSTALLED_APPS = [
     'members',
     'inventory',
     'borrowing',
-    'corsheaders'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    
 ]
 # for testing 
 CORS_ALLOW_ALL_ORIGINS = True
