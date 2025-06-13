d32892b271f88b7646acfe1d105885380f3dad68
AnjaliChoudhary99 <anjalichoudhary2610@gmail.com>
2025-06-13 15:48:45 +0530

    static files 2

--- library_system/settings.py
+++ library_system/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware' #(for static file serving)
+    'whitenoise.middleware.WhiteNoiseMiddleware', #(for static file serving)
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
